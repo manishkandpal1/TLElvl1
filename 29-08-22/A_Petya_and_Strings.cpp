36ec611f7a075d85afb0c8f9ec8e16a9aef31c0a
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2022-08-29 15:20:33 +0530

    Create A_Petya_and_Strings.cpp

---
+++ 29-08-22/A_Petya_and_Strings.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+string a,b;
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(NULL);
+   cin >> a >> b;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] < 92) {
+            a[i] += 32;
+        }
+        if (b[i] < 92) {
+            b[i] += 32;
+        }
+    }
+    if (a < b) {
+        cout << -1;
+    } else if (a > b) {
+        cout << 1;
+    } else if (a == b) {
+        cout << 0;
+    }
+    } 
