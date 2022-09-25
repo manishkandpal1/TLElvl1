145f3f64aae505a745abbf98783da364e9a48f65
Manish Kandpal <79416452+manishkandpal1@users.noreply.github.com>
2022-09-25 13:03:16 +0530

    Create 755A_PolandBallandHypothesis.cpp

---
+++ 25-09-22/755A_PolandBallandHypothesis.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+typedef long long ll;
+
+int m=1;
+void prime (int n)
+{
+    int i,rem,res,check;
+
+    check = 1, res = n*m + 1, i = sqrt(res);
+
+    if (res % 2 == 0)
+        check = 0;
+    else{
+        for (rem = 2; rem <= i; rem++)
+        {
+            if (res % rem == 0)
+            {
+                check = 0;
+                break;
+            }
+        }
+    }
+
+    if (check == 0)
+        cout << m << endl;
+    else {
+        m = m + 1;
+        prime (n);
+    }
+}
+
+int main (void){
+    int n,i,rem,check;
+    cin>>n;
+        if (n & 1){
+            if (n == 1)
+                cout << 3 << endl;
+            else
+                cout << 1 << endl;
+        }
+        else
+            prime (n);
+    return 0;
+}
